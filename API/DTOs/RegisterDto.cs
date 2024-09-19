a65c10c38dfec0f6a4b484aca4415e3406ae9d8f
sabrina-giancaspro <sabrinatwitter6@gmail.com>
2024-09-19 12:08:02 +0200

    new database migration and seeder population

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -10,5 +10,5 @@ public class RegisterDto
 
     [Required]
     [StringLength(8, MinimumLength = 4)]
-    public string Password { get; set; }
+    public required string Password { get; set; }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,9 @@
+using System.Net.Security;
 using API;
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +19,23 @@ app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
 
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
